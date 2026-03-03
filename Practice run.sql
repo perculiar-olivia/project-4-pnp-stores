dd6923f2dfc2844384e37ff75fe29cc0d3a19464
PerculiarOlivia <nomhletshehla20@gmail.com>
2026-03-03 22:07:46 +0200

    loaded the tables for 2025

---
+++ Practice run.sql
@@ -0,0 +1,6 @@
+SELECT * FROM [PnP_Store].[dbo].[pnp_sales_2025_01]
+SELECT * FROM [PnP_Store].[dbo].[pnp_sales_2025_02]
+
+SELECT *
+FROM [PnP_Store].[dbo].[pnp_sales_2025_01]
+	JOIN [PnP_Store].[dbo].[pnp_sales_2025_02]
